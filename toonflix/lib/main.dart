7317445758f11966ad79a4f4066bf9c750c22702
baekjontgae <dkvldzm@gmail.com>
2024-10-28 16:25:43 +0900

    a

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -12,76 +12,20 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  bool showTitle = true;
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          headlineLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(
-                  Icons.remove_red_eye,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    print('initstate');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    print('dispose!'); // 위젯이 위젯트리에서 제거되기전에 무언가 취소하고싶을때
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('buildstate');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
+      home: const Scaffold(
+        body: Center(),
       ),
     );
   }
