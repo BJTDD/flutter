adc5033a3e24ca046194554c40c45e60293ed486
baekjontgae <dkvldzm@gmail.com>
2024-10-29 17:30:30 +0900

    a

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -16,7 +16,7 @@ class App extends StatelessWidget {
           child: Column(
             children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(10, 40, 20, 0),
+                padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
                 child: Column(
                   children: [
                     Row(
@@ -53,7 +53,7 @@ class App extends StatelessWidget {
               Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
+                    padding: EdgeInsets.fromLTRB(20, 5, 0, 20),
                     child: Row(
                       children: [
                         Text(
@@ -72,7 +72,7 @@ class App extends StatelessWidget {
                         ),
                         SizedBox(width: 5),
                         Text(
-                          '17 18 19 20',
+                          '17 18 19 2', //오버플로우시 처리방법 알아보기...ㅇ=
                           style: TextStyle(
                             color: Colors.grey,
                             fontSize: 40,
@@ -86,10 +86,147 @@ class App extends StatelessWidget {
                   ),
                 ],
               ),
+              Column(
+                children: [
+                  MeetingWidget(
+                    color: Color(0xFCFEF754),
+                    text: 'DESIGN MEETING',
+                    start: '11.30',
+                    end: '12.20',
+                    team: 'ALEX, HELENA, NANA',
+                  ),
+                  MeetingWidget(
+                    color: Color(0xFF9C6BCE),
+                    text: 'DAILY PROJECT',
+                    start: '12.35',
+                    end: '14.10',
+                    team: 'ME, RICHARD, CIRY, 1, 2, 3, 4',
+                  ),
+                  MeetingWidget(
+                    color: Color(0xFFBCEE4B),
+                    text: 'WEEKLY PLANNING',
+                    start: '15.00',
+                    end: '16.30',
+                    team: 'DEN, NANA, MARK',
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     ); //materialapp cupertinoapp
+  }
+}
+
+class MeetingWidget extends StatelessWidget {
+  final Color color;
+  final String text;
+  final String start;
+  final String end;
+  final String team;
+
+  const MeetingWidget({
+    super.key,
+    required this.color,
+    required this.text,
+    required this.start,
+    required this.end,
+    required this.team,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(45),
+            ),
+            width: double.infinity,
+            height: 200,
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            start.split('.')[0],
+                            style: const TextStyle(
+                              fontSize: 25,
+                              height: 0.8,
+                            ),
+                          ),
+                          Text(start.split('.')[1]),
+                          const SizedBox(height: 5),
+                          const Text(
+                            '|',
+                            style: TextStyle(height: 0.2),
+                          ),
+                          const Text('|'),
+                          const SizedBox(height: 5),
+                          Text(
+                            end.split('.')[0],
+                            style: const TextStyle(
+                              fontSize: 25,
+                              height: 0.8,
+                            ),
+                          ),
+                          Text(end.split('.')[1]),
+                        ],
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          text,
+                          style: const TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                            height: 0.8,
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    const SizedBox(width: 50),
+                    Text(
+                      team.split(',')[0],
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    Text(
+                      team.split(',')[1],
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    Text(
+                      team.split(',')[2],
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
